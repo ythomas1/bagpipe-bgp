1cc409fcd6336985130169695e828564270c5657
Yannick Thomas <ythomas1.ext@orange.com>
2016-07-01 15:43:56 +0200

    Modify setupDataplaneForRemoteEndpoint and removeDataplaneForRemoteEndpoint signature to handle consistent hash sort order

--- bagpipe/bgp/vpn/ipvpn/mpls_linux_dataplane.py
+++ bagpipe/bgp/vpn/ipvpn/mpls_linux_dataplane.py
@@ -300,7 +300,7 @@ class MPLSLinuxVRFDataplane(VPNInstanceDataplane, LookingGlass):
 
     @logDecorator.logInfo
     def setupDataplaneForRemoteEndpoint(self, prefix, remotePE, label, nlri,
-                                        encaps):
+                                        encaps, lbConsistentHashOrder=0):
         prefix = str(prefix)
         try:
             with self._getRoute(prefix) as r:
@@ -316,7 +316,8 @@ class MPLSLinuxVRFDataplane(VPNInstanceDataplane, LookingGlass):
             self.add_route(req)
 
     @logDecorator.logInfo
-    def removeDataplaneForRemoteEndpoint(self, prefix, remotePE, label, nlri):
+    def removeDataplaneForRemoteEndpoint(self, prefix, remotePE, label, nlri,
+                                         encaps, lbConsistentHashOrder=0):
         prefix = str(prefix)
         try:
             with self._getRoute(prefix) as r:
